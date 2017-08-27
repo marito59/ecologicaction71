994204897d7679a0ace91255636a786faaf672e7
marito59 <maritore59@yahoo.fr>
2017-08-27 17:33:28 +0200

    Mise à jour
    simplification des fonctions
    amélioration de la présentation

--- themes/magazine-basic-child/page-documents.php
+++ themes/magazine-basic-child/page-documents.php
@@ -17,25 +17,35 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
-			
-			// select documents category
+
+			// begin modif CMA
+			// select documents category as root
 			$args = array('child_of' => 9, 'order_by' => 'name', 'order' => 'DESC');
 			$categories = get_categories( $args );
 			$prev_cat = '';
 			$cat_level = 0;
 
+			// pour chaque catégorie, affiche le nom puis les articles correspondants avec leurs attachements
 			foreach($categories as $category) { 
 				if ($prev_cat != $category) {
 					$cat_level++;
 					$prev_cat = $category;
 				}
-				//if ($category->count != 0) { ?>
-					<div class="category_name cat_level_<?php echo $catlevel;?>"><?php echo $category->name; ?></div>
+				?>
+				<div class="category_name cat_level_<?php echo $cat_level;?>"><?php echo $category->name; ?></div>
 			<?php
-					ecologic_docs($category->term_id);
-				//}
+				// display attachments (function from functions.php)
+				$second_query = new WP_Query(array('category__in'=>$category->term_id, 'post_type'=>'post', 'posts_per_page'=>-1, 'order'=>'ASC', 'orderby'=>'menu_order'));
+				
+				// The Loop
+				while( $second_query->have_posts() ) : $second_query->the_post();
+					get_template_part( 'template-parts/content', 'documents' );				
+				endwhile;
+		
+				wp_reset_query();
+				
 			}
-			
+			// end Modif CMA
 			comments_template( '', true );
 
 		endwhile; // end of the loop.
---
+++ themes/magazine-basic-child/template-parts/content-documents.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template used for displaying post content with attachements
+ *
+ * @since 3.0.0
+ */
+$class = bavotasan_article_class();
+?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( $class ); ?>>
+		<div class="document-thumb">
+		<?php
+		if ( has_post_thumbnail() ) {
+			$attr = array(
+				'class' => "documents-thumb",
+				'alt'   => "thumbnail"
+			);
+			the_post_thumbnail( 'document-thumb' ); 
+		} else { 
+		?> 	&nbsp; <?php
+		}?></div>
+		<div class="document-content"><?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+	    	<div class="entry-content">
+		  	  <?php the_content(); ?>
+			</div><!-- .entry-content -->
+		</div>
+<?php
+		$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' => null, 'post_parent' => get_the_ID()
+			, 'post_mime_type'=>array('application/pdf', 'application/msword', 'image/jpeg'), 'order'=>'ASC', 'orderby'=>'ID' ); 
+		$attachments = get_posts($args);
+		if ($attachments) {
+			echo('<ul>');
+				foreach ( $attachments as $attachment ) {
+					echo ('<li>');
+					//<a rel="alternate" href="http://www.goutdubio.fr/wp-content/uploads/2013/02/CHAMPVENT-certificat-2013.jpg" target="_blank">EARL Champvent � Certificat de conformit�&nbsp; de production biologique des produits commercialis�s</a>
+					
+					echo ('<a rel="alternate" href="');
+					echo wp_get_attachment_url($attachment->ID);
+					echo ('" target="blank">');
+					echo $attachment->post_title;
+					echo ('</a>');
+					echo ('</li>');
+				}
+			echo('</ul><br />');
+		}
+
+?>
+	</article><!-- #post-<?php the_ID(); ?> -->
